45bb9da8023a8b18c87f6ca3a535dca5fa5a4720
AnushkaD99 <92775945+AnushkaD99@users.noreply.github.com>
2023-02-02 00:27:33 +0530

    hashing vulnerabilities fixed

--- login.php
+++ login.php
@@ -56,6 +56,8 @@
                <center>
                    <p>Copyright &copy Lifestyle Store. All Rights Reserved. | Contact Us: +91 90000 00000</p>
                    <p>This website is developed by Sajal Agrawal</p>
+                   <p><?php $password = password_hash('Pathum@12345', PASSWORD_DEFAULT);
+                   echo $password ?></p>
                </center>
                </div>
            </footer>
--- login_submit.php
+++ login_submit.php
@@ -9,17 +9,22 @@
         <meta http-equiv="refresh" content="2;url=login.php" />
         <?php
     }
-    $password=md5(md5(mysqli_real_escape_string($con,$_POST['password'])));
+    $password = mysqli_real_escape_string($con,$_POST['password']);
     if(strlen($password)<6){
         echo "Password should have atleast 6 characters. Redirecting you back to login page...";
         ?>
         <meta http-equiv="refresh" content="2;url=login.php" />
         <?php
     }
-    $user_authentication_query="select id,email from users where email='$email' and password='$password'";
+    $user_authentication_query="SELECT * FROM users WHERE email='$email'";
     $user_authentication_result=mysqli_query($con,$user_authentication_query) or die(mysqli_error($con));
-    $rows_fetched=mysqli_num_rows($user_authentication_result);
-    if($rows_fetched==0){
+    //$rows_fetched=mysqli_num_rows($user_authentication_result);
+    $row=mysqli_fetch_array($user_authentication_result);
+    if(password_verify($password, $row['password'])){
+        $_SESSION['email']=$email;
+        $_SESSION['id']=$row['id'];  //user id
+        header('location: products.php');
+    }else{
         //no user
         //redirecting to same login page
         ?>
@@ -28,13 +33,8 @@
         </script>
         <meta http-equiv="refresh" content="1;url=login.php" />
         <?php
+        die($password);
         //header('location: login');
         //echo "Wrong email or password.";
-    }else{
-        $row=mysqli_fetch_array($user_authentication_result);
-        $_SESSION['email']=$email;
-        $_SESSION['id']=$row['id'];  //user id
-        header('location: products.php');
     }
-    
  ?>
