6125534936cee42724887cf6c66746fe539a0872
AnushkaD99 <92775945+AnushkaD99@users.noreply.github.com>
2023-02-11 20:23:43 +0530

    Update cart_add.php

--- cart_add.php
+++ cart_add.php
@@ -1,19 +1,10 @@
 <?php
     require 'connection.php';
+    //require 'header.php';
     session_start();
-    if(!isset($_SESSION['id'])){
-        header('location:index.php');
-        exit;
-    }
-    $item_id=filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+    $item_id=$_GET['id'];
     $user_id=$_SESSION['id'];
-    if($item_id === false){
-        header('location: products.php');
-        exit;
-    }
-    $add_to_cart_query="insert into users_items(user_id,item_id,status) values (?,?,?)";
-    $stmt = mysqli_prepare($con,$add_to_cart_query);
-    mysqli_stmt_bind_param($stmt, 'iis', $user_id, $item_id, "Added to cart");
-    $add_to_cart_result=mysqli_stmt_execute($stmt) or die(mysqli_error($con));
+    $add_to_cart_query="insert into users_items(user_id,item_id,status) values ('$user_id','$item_id','Added to cart')";
+    $add_to_cart_result=mysqli_query($con,$add_to_cart_query) or die(mysqli_error($con));
     header('location: products.php');
 ?>
