bb8eb69345162773d4d72470ca2752c30605cf0f
AnushkaD99 <92775945+AnushkaD99@users.noreply.github.com>
2023-02-11 21:28:39 +0530

    Update cart_add.php

--- cart_add.php
+++ cart_add.php
@@ -2,9 +2,11 @@
     require 'connection.php';
     //require 'header.php';
     session_start();
-    $item_id=$_GET['id'];
-    $user_id=$_SESSION['id'];
-    $add_to_cart_query="insert into users_items(user_id,item_id,status) values ('$user_id','$item_id','Added to cart')";
-    $add_to_cart_result=mysqli_query($con,$add_to_cart_query) or die(mysqli_error($con));
+    $item_id=mysqli_real_escape_string($con, $_GET['id']);
+    $user_id=mysqli_real_escape_string($con, $_SESSION['id']);
+    $add_to_cart_query="insert into users_items(user_id,item_id,status) values (?, ?, 'Added to cart')";
+    $stmt = mysqli_prepare($con, $add_to_cart_query);
+    mysqli_stmt_bind_param($stmt, "ii", $user_id, $item_id);
+    mysqli_stmt_execute($stmt);
     header('location: products.php');
 ?>
